1aabca9ceb8e53e5e280541ba8b256d14f47e43d
IbrahimMohamed23 <31793083+IbrahimMohamed23@users.noreply.github.com>
2017-09-09 11:15:32 +0200

    Create TestThing.java

---
+++ ITI_JMD/JD01/Introduction_to_OOP_Using_Java/L03_First_java_application/TestThing.java
@@ -0,0 +1,10 @@
+package thing; // When you add this, an error occurs in Run (not in compilation):
+              // Error: Could not find or load main class TestSome
+
+class TestThing
+{
+	public static void main(String[] arga)
+	{
+		System.out.println("Thing Success :)");
+	}
+}
